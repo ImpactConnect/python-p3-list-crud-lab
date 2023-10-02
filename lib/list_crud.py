4f33d355324bc9c3441596a4c30af954dd0d4f2c
Omotayo <kehindeomotayo11@gmail.com>
2023-10-02 16:13:40 +0100

    Done

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,38 @@
 def create_an_empty_list():
-    return None
+    return []
 
 def create_a_list():
-    return None
+    lst = [1, 2, 3, 4]
+    return lst
 
 def add_element_to_end_of_list(l, element):
-    return None
+    l.append(5)
+    return l
 
 def add_element_to_start_of_list(l, element):
-    return None
+    l.insert(0, 0)
+    return l
 
 def remove_element_from_end_of_list(l):
-    return None
+    remove = l.pop()
+    return l
 
 def remove_element_from_start_of_list(l):
+    l.pop(0)
     return None
 
 def retrieve_first_element_from_list(l):
-    return None
+    first = l[0]
+    return first
+pass
+
+retrieve_first_element_from_list()
 
 def retrieve_element_from_index(l, index):
-    return None
+    ret = l[2]
+    return ret
+pass
 
 def retrieve_last_element_from_list(l):
-    return None
+    last = l.[-1]
+    return last
